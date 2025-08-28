5beee39c066e40bcc24cb76d38a212afb5250928
wesley <wesleyhjpr@gmail.com>
2025-08-28 18:52:31 -0300

    Add hybrid flow support to authentication flow workspace
    - Updated package.json to include a new script for hybrid flow.
    - Added hybrid-flow to the workspaces array.

---
+++ authentication-flow/hybrid-flow/src/PrivateRoute.tsx
@@ -0,0 +1,13 @@
+import { useContext, type PropsWithChildren } from "react";
+import { AuthContext } from "./AuthProvider";
+import { Navigate } from "react-router-dom";
+
+export function PrivateRoute(props: PropsWithChildren) {
+  const authContext = useContext(AuthContext);
+
+  if (!authContext.auth) {
+    return <Navigate to="/login" />;
+  }
+
+  return props.children;
+}
